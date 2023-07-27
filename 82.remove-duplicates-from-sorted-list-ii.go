b9ec1decec42704b9c0006ceb37f9fe78af85a3b
ShintaNakama <shintanakama77@gmail.com>
2023-07-28 07:31:09 +0900

    fix: 82

--- 82.remove-duplicates-from-sorted-list-ii.go
+++ 82.remove-duplicates-from-sorted-list-ii.go
@@ -61,3 +61,45 @@ func deleteDuplicates2(head *ListNode) *ListNode {
 
 	return head
 }
+
+/*
+リンクリストにおいて「重複したノードを全て削除」するという問題を解決するには、特に「前」のノードの情報を保持しておくことが必要です。これは、リンクリストが一方向であるため、後から前のノードに戻ることができないからです。したがって、「前」のノードを追跡することで、重複したノードを削除した後にリンクリストを再接続することが可能となります。
+
+前のノードを追跡するには、通常は「ダミーノード」を使ったり、2つのポインタ（1つは現在のノードを指し、もう1つは前のノードを指す）を使用します。そして、リンクリストを通過する間、これらのポインタを適切に更新することで問題を解決します。
+
+*/
+
+func deleteDuplicates3(head *ListNode) *ListNode {
+	// Dummy node will be a "prev" node for head
+	// ダミーノードを作成し重複を全て削除するのでheadの前のノードを保持するために使用する
+	dummy := &ListNode{0, head}
+	// prevは前のノードでユニークなノードとなる
+	prev := dummy
+
+	for head != nil {
+		// If this is a beginning of duplicates sublist
+		// skip to the end of duplicates sublist
+		// ノードが重複するかチェック
+		if head.Next != nil && head.Val == head.Next.Val {
+			// 重複は全て削除するので、head.Nextがnilになるか重複しないノードまでheadを進める
+			// Skip to the end of duplicates sublist
+			for head.Next != nil && head.Val == head.Next.Val {
+				head = head.Next
+			}
+			// Skip duplicates
+			// ここで前のユニークなノードと重複を全て除いたノードをリンクさせる
+			// このタイミングのheadは重複したノードの最後のノードが入っているためhead.Nextを使用する
+			prev.Next = head.Next
+		} else {
+			// Otherwise, move predecessor
+			// 重複していない場合はprevを進める
+			prev = prev.Next
+		}
+		// Move forward
+		// headを進めて次のノードの重複をチェックする
+		head = head.Next
+	}
+
+	// dummy.Nextがユニークなノードの先頭となる
+	return dummy.Next
+}
